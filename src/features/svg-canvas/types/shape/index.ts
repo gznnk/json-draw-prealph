1ebdbee6e58526a9311bc1d1ae7972dee35734a1
masato nakagawa <m.nakagawa.3625@gmail.com>
2025-05-11 16:51:40 +0900

    format

--- src/features/svg-canvas/types/shape/index.ts
+++ src/features/svg-canvas/types/shape/index.ts
@@ -3,7 +3,10 @@
 export type { ConnectableData } from "./ConnectableData";
 export type { ConnectLineData } from "./ConnectLineData";
 export type { ConnectPointData } from "./ConnectPointData";
-export type { CreateDiagramType, DiagramDataOptions } from "./CreateDiagramType";
+export type {
+	CreateDiagramType,
+	DiagramDataOptions,
+} from "./CreateDiagramType";
 export type { EllipseData } from "./EllipseData";
 export type { GroupData } from "./GroupData";
 export type { ImageData } from "./ImageData";
