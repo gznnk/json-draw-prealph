5547204cec591a45f1b80027e2b9d986ea10e724
masato nakagawa <m.nakagawa.3625@gmail.com>
2025-06-17 20:26:27 +0900

    外接短径関数

--- src/features/svg-canvas/canvas/utils/calcItemsBoundingBox.ts
+++ src/features/svg-canvas/canvas/utils/calcItemsBoundingBox.ts
@@ -3,9 +3,8 @@ import type { Box } from "../../types/base/Box";
 import type { Diagram } from "../../types/data/catalog/Diagram";
 
 // Import utils.
-import { degreesToRadians } from "../../utils/math/common/degreesToRadians";
-import { rotatePoint } from "../../utils/math/points/rotatePoint";
 import { calcGroupOrientedBox } from "../../utils/shapes/group/calcGroupOrientedBox";
+import { calcItemBoundingBox } from "../../utils/math/geometry/calcItemBoundingBox";
 import { isItemableData } from "../../utils/validation/isItemableData";
 import { isTransformativeData } from "../../utils/validation/isTransformativeData";
 
@@ -37,25 +36,11 @@ export const calcItemsBoundingBox = (items: Diagram[]): Box => {
 				groupOrientedBox.y + groupOrientedBox.height,
 			);
 		} else if (isTransformativeData(item)) {
-			const radians = degreesToRadians(item.rotation);
-			const leftTop = rotatePoint(
-				item.x - item.width / 2,
-				item.y - item.height / 2,
-				item.x,
-				item.y,
-				radians,
-			);
-			const rightBottom = rotatePoint(
-				item.x + item.width / 2,
-				item.y + item.height / 2,
-				item.x,
-				item.y,
-				radians,
-			);
-			box.top = Math.min(box.top, leftTop.y);
-			box.left = Math.min(box.left, leftTop.x);
-			box.right = Math.max(box.right, rightBottom.x);
-			box.bottom = Math.max(box.bottom, rightBottom.y);
+			const itemBox = calcItemBoundingBox(item);
+			box.top = Math.min(box.top, itemBox.top);
+			box.left = Math.min(box.left, itemBox.left);
+			box.right = Math.max(box.right, itemBox.right);
+			box.bottom = Math.max(box.bottom, itemBox.bottom);
 		}
 	}
 
