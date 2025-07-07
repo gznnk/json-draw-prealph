ff89002af84af62def7ef593104658e3ba87b66d
masato nakagawa <m.nakagawa.3625@gmail.com>
2025-07-07 13:51:25 +0900

    不要な再エクスポート削除

--- src/features/svg-canvas/components/shapes/ConnectPoint/index.tsx
+++ src/features/svg-canvas/components/shapes/ConnectPoint/index.tsx
@@ -1,4 +1,2 @@
 export { ConnectPoint } from "./ConnectPoint";
-export { generateOptimalShapeToShapeConnection } from "../../../utils/shapes/connectPoint/generateOptimalShapeToShapeConnection";
-export { getLineDirection } from "../../../utils/shapes/connectPoint/getLineDirection";
 export type { Direction } from "./ConnectPointTypes";
