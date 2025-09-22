b129e121272a61cdb9a4fa8669c5548d72f72028
gznnk <32328338+gznnk@users.noreply.github.com>
2025-09-22 10:40:48 +0900

    付箋に最小幅

--- web/features/svg-canvas/constants/data/diagrams/StickyDefaultData.ts
+++ web/features/svg-canvas/constants/data/diagrams/StickyDefaultData.ts
@@ -11,6 +11,8 @@ export const StickyDefaultData: StickyData = CreateDefaultData<StickyData>({
 	properties: {
 		width: 160,
 		height: 120,
+		minWidth: 80,
+		minHeight: 60,
 		fill: "#fef9c3",
 		stroke: "#fef9c3",
 		strokeWidth: "1px",
